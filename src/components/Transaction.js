dbf9481185297a3a124735b7eef1e9501ff78aa5
chrisachinga <chris.achinga@student.moringaschool.com>
2022-09-25 20:08:36 +0300

    fetch data from API

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React from 'react'
 
-function Transaction() {
+function Transaction({ date, description, category, amount }) {
   return (
     <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
     </tr>
-  );
+  )
 }
 
-export default Transaction;
+export default Transaction
+
+Transaction.defaultProps = {
+  date: '01/01/2020',
+  description: 'No description',
+  category: 'No category',
+  amount: 0,
+}
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,28 +1,49 @@
-import React from "react";
-import Transaction from "./Transaction";
+import React, { useEffect, useState } from 'react'
+import Transaction from './Transaction'
 
 function TransactionsList() {
+  const [transactions, setTransactions] = useState([])
+
+  // get transations from API
+  useEffect(() => {
+    fetch('http://localhost:8001/transactions')
+      .then((response) => response.json())
+      .then((data) => {
+        setTransactions(data)
+      })
+  }, [])
+
   return (
-    <table className="ui celled striped padded table">
+    <table className='ui celled striped padded table'>
       <tbody>
         <tr>
           <th>
-            <h3 className="ui center aligned header">Date</h3>
+            <h3 className='ui center aligned header'>Date</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">Description</h3>
+            <h3 className='ui center aligned header'>Description</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">Category</h3>
+            <h3 className='ui center aligned header'>Category</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">Amount</h3>
+            <h3 className='ui center aligned header'>Amount</h3>
           </th>
         </tr>
         {/* render a list of <Transaction> components here */}
+        {/* {peopleInSpace.map((person) => person.name)} */}
+        {transactions.map((data) => (
+          <Transaction
+            key={data.id}
+            date={data.date}
+            description={data.description}
+            category={data.category}
+            amount={data.amount}
+          />
+        ))}
       </tbody>
     </table>
-  );
+  )
 }
 
-export default TransactionsList;
+export default TransactionsList
